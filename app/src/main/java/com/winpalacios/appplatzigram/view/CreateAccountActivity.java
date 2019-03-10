11f275471291c48c4186f14543adf9a54096dc7b
AP16014 <ap16014@ues.edu.sv>
2019-03-10 14:04:43 -0600

    creacion de activity create account

--- app/src/main/java/com/winpalacios/appplatzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/winpalacios/appplatzigram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.winpalacios.appplatzigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.winpalacios.appplatzigram.R;
 
@@ -11,6 +12,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolBar(getResources().getString(R.string.toolbar_create), false);
+    }
+
+    //metodo que nos ayuda a configurar el toolbar
+    public void showToolBar(String title, boolean upButton ){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
